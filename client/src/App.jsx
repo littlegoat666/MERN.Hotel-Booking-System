dcba137f830f7c6443bc0fc5ad464af5f49ec177
LittleGoat <jiiirayu2546@gmail.com>
2025-09-16 00:56:39 +0700

    Create Hotel Details Page
    - Create Navigation bar
    - Create Home Page
    - Add User Authentication using Clerk
    - Create Hotel List Page
    - Create Hotel Details Page
    - Create My Bookings Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import Footer from './components/Footer'
+import AllRooms from './pages/AllRooms'
+import RoomDetails from './pages/RoomDetails'
+import MyBookings from './pages/MyBookings'
 
 const App = () => {
     
@@ -13,8 +17,13 @@ const App = () => {
             <div className = 'min-h-[70vh]'>
                 <Routes>
                     <Route path = '/' element = { <Home /> } />
+                    <Route path = '/rooms' element = { <AllRooms /> } />
+                    <Route path='/rooms/:id' element={ <RoomDetails /> } />
+
+                    <Route path = '/my-bookings' element = { <MyBookings /> } />
                 </Routes>
             </div>
+            <Footer />
         </div>
     )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import SpecialOffers from '../components/SpecialOffers'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 const Home = () => {
     return (
         <>
             <Hero />
+            <FeaturedDestination />
+            <SpecialOffers />
+            <Testimonial />
+            <NewsLetter />
         </>
     )
 }
